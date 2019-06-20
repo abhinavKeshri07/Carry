e7ca82768e3b20a40d170f2c3a9b6fdec3340db0
abhinavKeshri07 <keshri.abhinav07@gmail.com>
2019-06-20 12:06:13 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/abhinav_11720007/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.abhinav_11720007;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.abhinav_11720007.fragments.carry;
+import com.example.abhinav_11720007.fragments.goal;
+import com.example.abhinav_11720007.fragments.task;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+private DrawerLayout drawer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.navView);
+        navigationView.setNavigationItemSelectedListener(this);
+        if(savedInstanceState == null) {
+            navigationView.setCheckedItem(R.id.carryItems);
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, new carry()).commit();
+        }
+    }
+    @Override
+    public void onBackPressed(){
+        if(drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else{
+            super.onBackPressed();
+        }
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.carryItems:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,new carry()).commit();
+                break;
+            case R.id.goals:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,new goal()).commit();
+                break;
+            case R.id.tasks:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,new task()).commit();
+                break;
+            case R.id.reportBug:
+                Toast.makeText(this, "report",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/abhinav_11720007/fragments/carry.java
@@ -0,0 +1,30 @@
+package com.example.abhinav_11720007.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.abhinav_11720007.R;
+
+public class carry extends Fragment {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.carry_fragment, container, false);
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
